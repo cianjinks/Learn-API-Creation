f593b100843965223ec87ff6df3dea910a74ec47
Cian Jinks <cjinks99@gmail.com>
2022-02-09 15:15:46 +0000

    Initial API tests

---
+++ server.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Created a local database called test-database-affiliate
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test-database-affiliate');
+var Bear = require('./app/models/bear');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 8080;
+
+// Routes
+var router = express.Router();
+
+// Run for all requests (here we can do any pre-processing and error handling)
+router.use(function(req, res, next) {
+    console.log('A request has come in!');
+    next(); 
+});
+
+// Accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// Actual database configuring routes
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created ' + req.body.name });
+        });
+
+    })
+
+    // get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function(req, res) {
+        Bear.find(function(err, bears) {
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
+    });
+
+// Parameterized routes
+router.route('/bears/:bear_id')
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    })
+
+    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+    .put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated to ' + req.body.name + ' !' });
+            });
+
+        })
+    })
+    // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+    .delete(function(req, res) {
+        var bear_name;
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            bear_name = bear.name
+        })
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function(err, bear) {
+            if (err)
+                res.send(err);
+            
+            res.json({ message: 'Successfully deleted ' + req.params.bear_id + ' - ' + bear_name });
+        });
+    });
+
+app.use('/api', router)
+//
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
